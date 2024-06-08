d30e80d2a34a8096919d916861aac29ba58a277e
nvivek42 <viveknarole100@gmail.com>
2024-06-08 12:44:00 +0530

    notes app commit 2

--- Apps/playground/NotesApp.js
+++ Apps/playground/NotesApp.js
@@ -1,15 +1,80 @@
 class NotesApp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        { id: 1, title: "note 1" },
+        { id: 2, title: "note 2" },
+        { id: 3, title: "note 3" },
+      ],
+    };
+
+    this.onAdd = this.onAdd.bind(this)
+    this.onRemove = this.onRemove.bind(this)
+  }
+
+    onAdd(note){
+      this.setState((prevState) => {
+        return {
+          notes: [
+            ...prevState.notes,
+            {
+              id: prevState.notes.length + 1,
+              title: note,
+            },
+          ],
+        }
+      })
+  }
+
   render() {
     return (
       <div>
         <Header title={this.props.title} subtitle={this.props.subtitle} />
+        <AddNote />
+        <NotesList notes={this.state.notes} />
       </div>
     );
   }
 }
 NotesApp.defaultProps = {
-    title: 'Notes App',
-     subtitle: 'what do you want to do today ? '
+  title: "Notes App",
+  subtitle: "what do you want to do today ? ",
+};
+
+class AddNote extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onAdd = this.onAdd.bind(this);
+  }
+
+  onAdd(e) {
+    const note = e.target.elements.note.value;
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="add-note">
+        <form onSubmit={this.onAdd}>
+          <div className="input-group mb-3">
+            <input
+              size="100"
+              name="note"
+              type="text"
+              className="form-control form-control"
+              placeholder="add notes here"
+            ></input>
+            <button className="btn btn-success" type="submit">
+              Add
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 class Header extends React.Component {
@@ -23,17 +88,46 @@ class Header extends React.Component {
   }
 }
 
-class Header1 extends React.Component {
+class NotesList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onRemove = this.onRemove.bind(this);
+  }
+
+  onRemove(note) {}
+
   render() {
     return (
-      <div>
-        <h1>this is div1</h1>
+      <div className="notes-list">
+        {this.props.notes.map((note) => {
+          return (
+            <div classname="container">
+              <div className="input-group mb-3">
+                <input
+                  type="text"
+                  value={note.title}
+                  className="form-control-lg"
+                  size="100"
+                  readOnly
+                ></input>
+                <button
+                  onClick={() => {
+                    this.onRemove(note);
+                  }}
+                  className="btn btn-danger"
+                  type="button"
+                >
+                  Delete
+                </button>
+                <p></p>
+              </div>
+            </div>
+          );
+        })}
       </div>
     );
   }
 }
 
-ReactDOM.render(
-  <NotesApp  />,
-  document.getElementById("app")
-);
+ReactDOM.render(<NotesApp />, document.getElementById("app"));
--- Apps/public/scripts/app.js
+++ Apps/public/scripts/app.js
@@ -1,6 +1,8 @@
-'use strict';
+"use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -11,19 +13,41 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 var NotesApp = function (_React$Component) {
   _inherits(NotesApp, _React$Component);
 
-  function NotesApp() {
+  function NotesApp(props) {
     _classCallCheck(this, NotesApp);
 
-    return _possibleConstructorReturn(this, (NotesApp.__proto__ || Object.getPrototypeOf(NotesApp)).apply(this, arguments));
+    var _this = _possibleConstructorReturn(this, (NotesApp.__proto__ || Object.getPrototypeOf(NotesApp)).call(this, props));
+
+    _this.state = {
+      notes: [{ id: 1, title: "note 1" }, { id: 2, title: "note 2" }, { id: 3, title: "note 3" }]
+    };
+
+    _this.onAdd = _this.onAdd.bind(_this);
+    _this.onRemove = _this.onRemove.bind(_this);
+    return _this;
   }
 
   _createClass(NotesApp, [{
-    key: 'render',
+    key: "onAdd",
+    value: function onAdd(note) {
+      this.setState(function (prevState) {
+        return {
+          notes: [].concat(_toConsumableArray(prevState.notes), [{
+            id: prevState.notes.length + 1,
+            title: note
+          }])
+        };
+      });
+    }
+  }, {
+    key: "render",
     value: function render() {
       return React.createElement(
-        'div',
+        "div",
         null,
-        React.createElement(Header, { title: this.props.title, subtitle: this.props.subtitle })
+        React.createElement(Header, { title: this.props.title, subtitle: this.props.subtitle }),
+        React.createElement(AddNote, null),
+        React.createElement(NotesList, { notes: this.state.notes })
       );
     }
   }]);
@@ -32,12 +56,63 @@ var NotesApp = function (_React$Component) {
 }(React.Component);
 
 NotesApp.defaultProps = {
-  title: 'Notes App',
-  subtitle: 'what do you want to do today ? '
+  title: "Notes App",
+  subtitle: "what do you want to do today ? "
 };
 
-var Header = function (_React$Component2) {
-  _inherits(Header, _React$Component2);
+var AddNote = function (_React$Component2) {
+  _inherits(AddNote, _React$Component2);
+
+  function AddNote(props) {
+    _classCallCheck(this, AddNote);
+
+    var _this2 = _possibleConstructorReturn(this, (AddNote.__proto__ || Object.getPrototypeOf(AddNote)).call(this, props));
+
+    _this2.onAdd = _this2.onAdd.bind(_this2);
+    return _this2;
+  }
+
+  _createClass(AddNote, [{
+    key: "onAdd",
+    value: function onAdd(e) {
+      var note = e.target.elements.note.value;
+      e.preventDefault();
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        { className: "add-note" },
+        React.createElement(
+          "form",
+          { onSubmit: this.onAdd },
+          React.createElement(
+            "div",
+            { className: "input-group mb-3" },
+            React.createElement("input", {
+              size: "100",
+              name: "note",
+              type: "text",
+              className: "form-control form-control",
+              placeholder: "add notes here"
+            }),
+            React.createElement(
+              "button",
+              { className: "btn btn-success", type: "submit" },
+              "Add"
+            )
+          )
+        )
+      );
+    }
+  }]);
+
+  return AddNote;
+}(React.Component);
+
+var Header = function (_React$Component3) {
+  _inherits(Header, _React$Component3);
 
   function Header() {
     _classCallCheck(this, Header);
@@ -46,18 +121,18 @@ var Header = function (_React$Component2) {
   }
 
   _createClass(Header, [{
-    key: 'render',
+    key: "render",
     value: function render() {
       return React.createElement(
-        'div',
-        { className: 'title' },
+        "div",
+        { className: "title" },
         React.createElement(
-          'h1',
+          "h1",
           null,
           this.props.title
         ),
         React.createElement(
-          'h4',
+          "h4",
           null,
           this.props.subtitle
         )
@@ -68,31 +143,63 @@ var Header = function (_React$Component2) {
   return Header;
 }(React.Component);
 
-var Header1 = function (_React$Component3) {
-  _inherits(Header1, _React$Component3);
+var NotesList = function (_React$Component4) {
+  _inherits(NotesList, _React$Component4);
 
-  function Header1() {
-    _classCallCheck(this, Header1);
+  function NotesList(props) {
+    _classCallCheck(this, NotesList);
 
-    return _possibleConstructorReturn(this, (Header1.__proto__ || Object.getPrototypeOf(Header1)).apply(this, arguments));
+    var _this4 = _possibleConstructorReturn(this, (NotesList.__proto__ || Object.getPrototypeOf(NotesList)).call(this, props));
+
+    _this4.onRemove = _this4.onRemove.bind(_this4);
+    return _this4;
   }
 
-  _createClass(Header1, [{
-    key: 'render',
+  _createClass(NotesList, [{
+    key: "onRemove",
+    value: function onRemove(note) {}
+  }, {
+    key: "render",
     value: function render() {
+      var _this5 = this;
+
       return React.createElement(
-        'div',
-        null,
-        React.createElement(
-          'h1',
-          null,
-          'this is div1'
-        )
+        "div",
+        { className: "notes-list" },
+        this.props.notes.map(function (note) {
+          return React.createElement(
+            "div",
+            { classname: "container" },
+            React.createElement(
+              "div",
+              { className: "input-group mb-3" },
+              React.createElement("input", {
+                type: "text",
+                value: note.title,
+                className: "form-control-lg",
+                size: "100",
+                readOnly: true
+              }),
+              React.createElement(
+                "button",
+                {
+                  onClick: function onClick() {
+                    _this5.onRemove(note);
+                  },
+                  className: "btn btn-danger",
+                  type: "button"
+                },
+                "Delete"
+              ),
+              React.createElement("p", null)
+            )
+          );
+        })
       );
     }
   }]);
 
-  return Header1;
+  return NotesList;
 }(React.Component);
 
 ReactDOM.render(React.createElement(NotesApp, null), document.getElementById("app"));
